51460589fe162ea6e6de3d2e401e5a7cba7bca19
AlbertoAncilotto <ancilottoalberto@gmail.com>
2025-04-11 10:24:27 +0200

    updated inference code

--- webcam.py
+++ webcam.py
@@ -2,7 +2,7 @@ import cv2
 from transformer import TransformerNetworkXiNet
 import torch
 import utils
-from model_profiler import model_info
+from utils import model_info
 import numpy as np
 
 
@@ -12,15 +12,8 @@ WIDTH = 160
 HEIGHT = 160
 
 def webcam(style_transform_paths, width=1280, height=720):
-    """
-    Captures and saves an image, perform style transfer, and again saves the styled image.
-    Reads the styled image and show in window. 
-    """
-    # Device
     device = ("cuda" if torch.cuda.is_available() else "cpu")
 
-    # Load Transformer Network
-    print("Loading Transformer Network")
     nets = []
     for style_transform_path in style_transform_paths:
         net = TransformerNetworkXiNet(lite=False, alpha=0.75, bn_instead_of_in=True)
@@ -31,24 +24,18 @@ def webcam(style_transform_paths, width=1280, height=720):
         print("Done Loading Transformer Network")
 
 
-    # Set webcam settings
     cam = cv2.VideoCapture(0)
     # cam = cv2.VideoCapture('picture_frame\BNS2023.mp4')
     cam.set(3, width)
     model_id=0
-    # Main loop
     added_noise=(np.random.rand(height, width, 3)-0.5)*20
     with torch.no_grad():
         while True:
-            # Get webcam input
             ret_val, img = cam.read()
-
-            # Mirror 
             img = cv2.resize(img, (width, height)).astype(np.float32)
-            # img+= added_noise
-            img = cv2.flip(img, 1)/300#/260.0
+            img+= added_noise
+            img = cv2.flip(img, 1)/300
 
-            # Generate image
             content_tensor = utils.itot(img).to(device)
             content_tensor = torch.clip(content_tensor, 0.0, 1.0)*255
             print('content batch stats:',torch.min(content_tensor), torch.max(content_tensor), torch.mean(content_tensor))
@@ -58,7 +45,6 @@ def webcam(style_transform_paths, width=1280, height=720):
             if (PRESERVE_COLOR[model_id]):
                 generated_image = utils.transfer_color((img*255.0).astype(np.uint8), generated_image.astype(np.uint8))
 
-            # Show webcam
             cv2.imshow('Demo webcam', generated_image.astype(np.uint8))
             key = cv2.waitKey(1)
             if key != -1: 
@@ -75,9 +61,8 @@ def webcam(style_transform_paths, width=1280, height=720):
     modelpath = 'style_transfer.onnx'
     input_names = [ "input1" ]
     output_names = [ "output1" ]
-    # Free-up memories
-    torch.onnx.export(net, content_tensor, modelpath, export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=11,          # the ONNX version to export the model to
+    torch.onnx.export(net, content_tensor, modelpath, export_params=True,       
+                  opset_version=11, 
                   do_constant_folding=True, 
                   input_names=input_names, output_names=output_names
                   )
--- webcam_onnx.py
+++ webcam_onnx.py
@@ -3,39 +3,27 @@ import numpy as np
 import onnxruntime as ort
 import utils
 
-STYLE_TRANSFORM_PATHS = ['style_transfer_gogh_256a100.onnx']
+STYLE_TRANSFORM_PATHS = ['style_transfer.onnx']
 PRESERVE_COLOR = [False]
 WIDTH = 256
 HEIGHT = 256
 
 
 def webcam(style_transform_paths, width=1280, height=720):
-    """
-    Captures and saves an image, perform style transfer, and again saves the styled image.
-    Reads the styled image and show in window. 
-    """
-    # Load ONNX model
     ort_sessions=[]
     for P in STYLE_TRANSFORM_PATHS:
         ort_sessions.append(ort.InferenceSession(P))
     input_name = ort_sessions[0].get_inputs()[0].name
     output_name = ort_sessions[0].get_outputs()[0].name
 
-    # Set webcam settings
     cam = cv2.VideoCapture(0)
     cam.set(3, width)
     model_id = 0
 
-    # Main loop
     while True:
-        # Get webcam input
         ret_val, img = cam.read()
-
-        # Mirror 
         img = cv2.flip(img, 1)
         img = cv2.resize(img, (width, height))
-
-        # Generate image
         
         content_tensor = np.transpose(img, (2, 0, 1))
         content_tensor = np.expand_dims(content_tensor, axis=0).astype(np.float32)/255.0
@@ -48,13 +36,11 @@ def webcam(style_transform_paths, width=1280, height=720):
             generated_image = utils.transfer_color(img, generated_image)
 
         generated_image = generated_image / 255
-
-        # Show webcam
         cv2.imshow('Demo webcam', generated_image)
         key = cv2.waitKey(1)
         if key != -1: 
             if key == 27:
-                break  # esc to quit
+                break 
             else:
                 model_id += 1
                 model_id %= len(style_transform_paths)
